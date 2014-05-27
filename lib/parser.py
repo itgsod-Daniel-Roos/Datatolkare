18ff0d65b5fca3e8ac2dcda017fa705a694291f9
itgsod-Daniel-Roos <daniel.roos@elev.it-gymnasiet.se>
2014-05-27 10:57:41 +0200

    CHANGE OF ORDER
    A NEW LEADER HAS BEEN CHOSEN!!

--- lib/parser.py
+++ lib/parser.py
@@ -1,9 +1,3 @@
-def load_weather_file(file):
-    file = open(weather.dat())
-
-    pass
-
-
 def split_line(split_line):
     if split_line == '':
         raise ValueError('can not parse empty line')
@@ -15,6 +9,13 @@ def encode_line():
 
 
 def find_biggest_variation():
+    pass
+
+
+def load_weather_file(myfile):
+    with open(myfile, 'w') as weatherfile:
+        weatherdata = weatherfile.readlines()
+
     pass
 
 
